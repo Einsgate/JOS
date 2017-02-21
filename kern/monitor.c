fcb5c323f7008b4c4ca45ccc0bebdffe64c72855
Einsgate <einsgate@163.com>
2017-02-21 12:25:28 +0800

    little fix

--- kern/monitor.c
+++ kern/monitor.c
@@ -67,13 +67,15 @@ mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 	do{
 		eip = ebp[1];
 		if(debuginfo_eip(eip, &info) == 0){
-			cprintf("  ebp %08x  eip %08x  args %08x %08x %08x %08x %08x\n",				ebp, eip, ebp[2], ebp[3], ebp[4], ebp[5], ebp[6]);
+			cprintf("  ebp %08x  eip %08x  args %08x %08x %08x %08x %08x\n",				
+				ebp, eip, ebp[2], ebp[3], ebp[4], ebp[5], ebp[6]);
 			cprintf("         %s:%d: %.*s+%d\n", info.eip_file,
 				info.eip_line, info.eip_fn_namelen, info.eip_fn_name, 
 				eip-info.eip_fn_addr);
 		}
 		else {
-			cprintf("  ebp %08x  eip %08x  args %08x %08x %08x %08x %08x\n",				ebp, eip, ebp[2], ebp[3], ebp[4], ebp[5], ebp[6]);
+			cprintf("  ebp %08x  eip %08x  args %08x %08x %08x %08x %08x\n",				
+				ebp, eip, ebp[2], ebp[3], ebp[4], ebp[5], ebp[6]);
 			cprintf("Debuginfo of eip not found!\n");
 		}
 		ebp = (uint32_t *)ebp[0];
